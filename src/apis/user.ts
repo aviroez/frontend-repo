459750d057fa24e2925de89f15c965cd4b1334d1
aviroez <avi.roez@gmail.com>
2024-12-10 13:22:47 +0700

    fix user & remove swr

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -1,10 +1,10 @@
-// export interface User {
-//     uid?: string;
-//     name?: string;
-//     email: string | null;
-//     age?: number;
-//     salary?: number;
-//     joinedDate?: string;
-// } 
+export interface User {
+    uid?: string;
+    name?: string;
+    email: string | null;
+    age?: number;
+    salary?: number;
+    joinedDate?: string;
+} 
 
-export type { User } from "@buddy-turborepo/shared";
+// export type { User } from "@buddy-turborepo/shared";
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -13,8 +13,7 @@ import { fetchUserById } from "@/apis/userApi";
 import { onAuthStateChanged } from "firebase/auth";
 import { firebaseAuth } from "@/lib/firebaseConfig";
 import { useRouter } from "next/navigation";
-// import { User } from "@/apis/user"; // use from turborepo instead
-import { User } from "@buddy-turborepo/shared";
+import { User } from "@/apis/user";
 import AlertDialog from "@/components/AlertDialog";
 
 export default function Profile() {
